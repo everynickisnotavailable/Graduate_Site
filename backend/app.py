c883a231979c2f7871fe4d92ed6901957144f669
everynickisnotavailable <nyshmysh3@gmail.com>
2026-01-02 21:18:49 +0300

    vite+vue

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,9 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-# Инициализация приложения
 app = Flask(__name__)
 CORS(app)  # Разрешаем запросы с любого фронтенда
 
-# Маршрут для проверки здоровья сервера
 @app.route('/api/ping', methods=['GET'])
 def ping():
     return jsonify({
